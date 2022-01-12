051c7090d10d6c0453253b05e4400d04c314f39b
Sudeep <sudeepanand1986@gmail.com>
2022-01-12 09:27:45 +0100

    updated restapi file

--- src/test/java/rahulshettyacademy/Mavenjavanew/RESTAPITest.java
+++ src/test/java/rahulshettyacademy/Mavenjavanew/RESTAPITest.java
@@ -9,6 +9,7 @@ public class RESTAPITest {
 		System.out.println("postJira");
 		System.out.println("postJira2");
 		System.out.println("postJira3");
+		System.out.println("hello updating");
 	}
 
 	@Test
